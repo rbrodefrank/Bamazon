2431f91822be064e2a9822b7688a2d41bce58f21
Ryan BrodeFrank <ryanafrank@gmail.com>
2018-12-15 22:25:57 -0600

    added increaseStock function to bM.js, increases inventory in database

--- bamazonManager.js
+++ bamazonManager.js
@@ -69,10 +69,10 @@ function main() {
                 lowStockList();
                 break;
             case "Add to Inventory":
-
+                increaseStock();
                 break;
             case "Add New Product":
-
+                addItem();
                 break;
             case "Exit":
                 connection.end();
@@ -106,4 +106,84 @@ function lowStockList() {
 
         main();
     });
+}
+
+function increaseStock() {
+    inquirer.prompt([
+        {
+            name: "product",
+            message: `\nWhat is the product id you want to add stock to?\nInput "back" to go to main menu.`,
+            type: "input"
+        }
+    ]).then(function (answers) {
+        var product = -4;
+        // console.log(answers.product);
+
+        if (answers.product.toLowerCase() == "back") {
+            main();
+        } else {
+            var int = parseInt(answers.product);
+            for (var i = 0; i < stock.length; i++) {
+                // console.log(`id: ${stock[i].item_id}`);
+                if (int == stock[i].item_id) {
+                    product = stock[i];
+                    console.log(`Product Selected: ${stock[i].product_name}\nProduct Stock: ${stock[i].stock_quantity}`);
+                    break;
+                }
+            }
+
+            if (product < 0) {
+                console.log("Invalid Input. Please input product ID.");
+                increaseStock();
+            } else {
+                stockQuantity(product);
+            }
+        }
+    });
+}
+
+function stockQuantity(product) {
+    inquirer.prompt([
+        {
+            name: "quantity",
+            type: "input",
+            message: `How many more '${product.product_name}' would you like to get?`
+        }
+    ]).then(function (answers) {
+        var num = parseInt(answers.quantity);
+        if (!Number.isNaN(num) && num > 0) {
+            connection.query(
+                "SELECT * FROM products WHERE ?",
+                {
+                    item_id: product.item_id
+                },
+                function(error, res) {
+                    if(error) throw error;
+                    var currentStock = res[0].stock_quantity;
+                    connection.query(
+                        "UPDATE products SET ? WHERE ?",
+                        [
+                            {stock_quantity: currentStock + num},
+                            {item_id: product.item_id}
+                        ],
+                        function(error, res) {
+                            if(error) throw error;
+                            console.log(`Increased ${product.product_name} by ${num}. Total stock is now ${num + currentStock}`);
+                            main();
+                        }
+                    )
+                }
+            );
+        } else if (num === 0) {
+            console.log(`0 ${product.product_name} added to inventory.`);
+        } else {
+            console.log("Invalid Input: Please input a number");
+            stockQuantity(product);
+        }
+    });
+}
+
+function addItem() {
+    console.log("Not implemented");
+    main();
 }
